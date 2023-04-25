68b7e008dc38e82370df6d6b60ee987e856995c8
Leandro Sánchez <leanmsan@gmail.com>
2023-04-25 15:47:09 -0300

    Creacion y borrado de datos
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- modularizacion/comentarios/models.py
+++ modularizacion/comentarios/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 # Clase comentario
-class comentarios(models.Model):
+class Comentario(models.Model):
     nombre = models.CharField(max_length=255, null=False)
     score = models.IntegerField(default=3)
-    comentario = models.TimeField(max_length=1000, null=True)
-    fecha = models.DateField(null=True)
+    comentario = models.TextField(max_length=1000, null=True)
 
     def __str__(self):
         return self.nombre
--- modularizacion/comentarios/views.py
+++ modularizacion/comentarios/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Comentario
 
 # Create your views here.
+def test(request):
+    return HttpResponse("Funciona Correctamente")
+
+def create(request):
+    #comment = Comentario(nombre="Leandro", score=5, comentario='Este es un comentario')
+    #comment.save()
+    comment = Comentario.objects.create(nombre="Leandro")
+    return HttpResponse("ruta para probar la creacion de modelos")
+
+def delete(request):
+    #comment = Comentario.objects.get(id=1)
+    #comment.delete()
+    Comentario.objects.filter(id=2).delete()
+    return HttpResponse("Borrar un comentario")
