3702a1b2a2fc87cafc477b46516a4e208acf8c2c
Leandro Sánchez <leanmsan@gmail.com>
2023-04-30 18:54:02 -0300

    Validacion de Formularios
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- formularios_django/formularios_django/forms.py
+++ formularios_django/formularios_django/forms.py
@@ -9,3 +9,10 @@ class ContactForm(forms.Form):
     name = forms.CharField(label='Nombre: ', max_length=50, widget=forms.TextInput(attrs={'class': 'form-control'}))
     email = forms.EmailField(label='Email', max_length=50, widget=forms.EmailInput(attrs={'class': 'form-control'}))
     message = forms.CharField(label='Mensaje', widget=forms.Textarea(attrs={'class': 'form-control'}))
+
+    def clean_name(self):
+        name = self.cleaned_data.get("name")
+        if name != 'Open':
+            raise forms.ValidationError("Solo el valor Open esta permitido en este campo")
+        else:
+            return name
--- formularios_django/formularios_django/views.py
+++ formularios_django/formularios_django/views.py
@@ -13,5 +13,13 @@ def goal(request):
     return HttpResponse(request.POST['name'])
 
 def widget (request):
-    form = ContactForm()
-    return render(request, 'widget.html', {'form': form})
+    if request.method == 'GET':
+        form = ContactForm()
+        return render(request, 'widget.html', {'form': form})
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            return HttpResponse("Valido")
+        else:
+            return render(request, 'widget.html', {'form': form})
