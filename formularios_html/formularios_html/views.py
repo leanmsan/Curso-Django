f18eb1251fdd89a40182d680092af91b1f160642
Leandro Sánchez <leanmsan@gmail.com>
2023-04-30 16:02:58 -0300

    Formulario POST
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- formularios_html/formularios_html/views.py
+++ formularios_html/formularios_html/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def form(request):
+def getform(request):
     return render(request, 'form.html', {})
 
-def goal(request):
+def getgoal(request):
     if request.method != 'GET':
         return HttpResponse("Error")
     
     name = request.GET['name']
 
     return render(request, 'success.html', {'name':name})
+
+def postform(request):
+    return render(request, 'postform.html', {})
+
+def postgoal(request):
+    if request.method != 'POST':
+        return HttpResponse("El metodo GET no esta soportado para esta ruta")
+    
+    info = request.POST['info']
+    return render(request, 'postsuccess.html', {'info':info})
