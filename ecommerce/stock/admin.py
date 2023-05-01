a492dcfa1eced75f3a82fb34385818c9dbed814c
Leandro Sánchez <leanmsan@gmail.com>
2023-05-01 17:30:47 -0300

    Model Admin
    Signed-off-by: Leandro Sánchez <leanmsan@gmail.com>

--- ecommerce/stock/admin.py
+++ ecommerce/stock/admin.py
@@ -1,5 +1,10 @@
-from django.contrib import admin
+from django.contrib import admin 
 from .models import Product
 
 # Register your models here.
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'short_description', 'stock',)
+    search_fields = ('name', 'short_description')
+    list_filter = ('discount_until','stock',)
+    date_hierarchy = 'discount_until'
+admin.site.register(Product, ProductAdmin)
